48a07cc5e9f07d7c54711c00aeda4a1144136d41
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-08-13 12:30:16 +0900

    Dynamic or Static Rendering

--- file-router/app/blog/page.tsx
+++ file-router/app/blog/page.tsx
@@ -1,12 +1,19 @@
 function sleep() {
-  return new Promise((resolve) => setTimeout(resolve, 5000));
+  return new Promise((resolve) => setTimeout(resolve, 1000));
 }
 
 const blog = async () => {
   await sleep();
+
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", { 
+    cache: "no-store",
+  });
+
+  console.log(res.json());
+
   return (
     <div>
-      <h1>blog</h1>
+      <h1>blog {new Date().toLocaleTimeString()}</h1>
     </div>
   );
 };
