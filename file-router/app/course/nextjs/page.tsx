742ca11b4be6225318d6864375094880388a3c48
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-08-13 12:17:13 +0900

    File Based Router

---
+++ file-router/app/course/nextjs/page.tsx
@@ -0,0 +1,9 @@
+const nextjs = () => {
+  return (
+    <div>
+      <h1>Nextjs</h1>
+    </div>
+  );
+};
+
+export default nextjs;
---
+++ file-router/app/course/page.tsx
@@ -0,0 +1,9 @@
+const course = () => {
+  return (
+    <div>
+      <h1>All Courses</h1>
+    </div>
+  );
+};
+
+export default course;
