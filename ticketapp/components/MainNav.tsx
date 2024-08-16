5ea4a231841618f353f95568a4dabe52487656e7
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-08-16 13:37:22 +0900

    dark mode

--- ticketapp/components/MainNav.tsx
+++ ticketapp/components/MainNav.tsx
@@ -1,17 +1,18 @@
 import Link from "next/link";
 import React from 'react';
+import ToggleMode from "./ToggleMode";
 
 const MainNav = () => {
   return (
   <div className="flex justify-between">
     <div className="flex items-center gap-2">
-    <Link href="/">Dashboard</Link>
-    <Link href="/tickets">Tickets</Link>
-    <Link href="/users">Users</Link>
+      <Link href="/">Dashboard</Link>
+      <Link href="/tickets">Tickets</Link>
+      <Link href="/users">Users</Link>
     </div>
     <div className="flex items-center gap-2">
-    <Link href="/">Logout</Link>
-    <Link href="/">Dark</Link>
+      <Link href="/">Logout</Link>
+      <ToggleMode />
     </div>
   </div>
   );
