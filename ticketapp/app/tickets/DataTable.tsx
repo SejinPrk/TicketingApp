59f489be6636a385e0216a2570e1bfc10b04555a
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-08-18 00:11:05 +0900

    Priority Component

--- ticketapp/app/tickets/DataTable.tsx
+++ ticketapp/app/tickets/DataTable.tsx
@@ -1,5 +1,6 @@
 import TicketStatusBadge from '@/components/TicketStatusBadge';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import TicketPriority from '@/components/ui/TicketPriority';
 import { Ticket } from '@prisma/client'
 import React from 'react'
 
@@ -15,8 +16,12 @@ const DataTable = ({tickets} : Props) => {
           <TableHeader>
             <TableRow>
               <TableHead>Title</TableHead>
-              <TableHead>Status</TableHead>
-              <TableHead>Priority</TableHead>
+              <TableHead>
+                <div className='flex justify-center'>Status</div>  
+              </TableHead>
+              <TableHead>
+                <div className='flex justify-center'>Priority</div>
+              </TableHead>
               <TableHead>Created At</TableHead>
             </TableRow>
           </TableHeader>
@@ -24,8 +29,16 @@ const DataTable = ({tickets} : Props) => {
             {tickets ? tickets.map((ticket) => (
               <TableRow key={ticket.id} data-href="/">
                 <TableCell>{ticket.title}</TableCell>
-                <TableCell><TicketStatusBadge status={ticket.status} /></TableCell>
-                <TableCell>{ticket.priority}</TableCell>
+                <TableCell>
+                  <div className='flex justify-center'>
+                    <TicketStatusBadge status={ticket.status} />
+                  </div>
+                </TableCell>
+                <TableCell>
+                  <div className='flex justify-center'>
+                    <TicketPriority priority={ticket.priority} />
+                  </div>
+                </TableCell>
                 <TableCell>
                   {ticket.createdAt.toLocaleDateString("en-US", {
                     year: "2-digit",
