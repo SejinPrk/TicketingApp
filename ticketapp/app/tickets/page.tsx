cb6d5934da08ed58b33b496b119d7f92b6a0d8a2
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-08-20 22:46:23 +0900

    Pagination Component & Server Pagination

--- ticketapp/app/tickets/page.tsx
+++ ticketapp/app/tickets/page.tsx
@@ -3,17 +3,36 @@ import prisma from '@/prisma/db';
 import DataTable from './DataTable';
 import Link from "next/link";
 import { buttonVariants } from '@/components/ui/button';
+import Pagination from '@/components/Pagination';
+import { pages } from 'next/dist/build/templates/app-page';
 
-const Tickets = async () => {
-  const tickets = await prisma.ticket.findMany();
+interface SearchParams {
+  page: string;
+}
+
+const Tickets = async ({searchParams}: {searchParams: SearchParams}) => {
+  const pageSize = 10;
+  const page = parseInt(searchParams.page) || 1;
+  const ticketCount = await prisma.ticket.count();
+
+  const tickets = await prisma.ticket.findMany({
+    take: pageSize,
+    skip: (page - 1) * pageSize,
+  });
 
   return (
-    <div>
+    <div className='mt-4'>
       <Link 
         href="/tickets/new" 
         className={buttonVariants({variant: "default" })}
-      >New Ticket</Link>
+      >
+        New Ticket
+      </Link>
       <DataTable tickets={tickets} />
+      <Pagination 
+        itemCount={ticketCount} 
+        pageSize={pageSize} 
+        currentPage={page}/>
     </div>
   );
 };
