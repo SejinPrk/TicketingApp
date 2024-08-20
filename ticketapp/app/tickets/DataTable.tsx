15f7139db1feadd0d6e558948af50f6d8b780b75
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-08-20 15:23:07 +0900

    Linking to Tickets

--- ticketapp/app/tickets/DataTable.tsx
+++ ticketapp/app/tickets/DataTable.tsx
@@ -3,6 +3,7 @@ import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@
 import TicketPriority from '@/components/TicketPriority';
 import { Ticket } from '@prisma/client'
 import React from 'react'
+import Link from 'next/link';
 
 interface Props {
   tickets: Ticket[]
@@ -28,7 +29,7 @@ const DataTable = ({tickets} : Props) => {
           <TableBody>
             {tickets ? tickets.map((ticket) => (
               <TableRow key={ticket.id} data-href="/">
-                <TableCell>{ticket.title}</TableCell>
+                <TableCell><Link href={`/tickets/${ticket.id}`}>{ticket.title}</Link></TableCell>
                 <TableCell>
                   <div className='flex justify-center'>
                     <TicketStatusBadge status={ticket.status} />
