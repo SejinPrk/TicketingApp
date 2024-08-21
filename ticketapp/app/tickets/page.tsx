dd7c9bc2472d0254fe5bd1d09971a61d39ce3dc4
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-08-21 22:28:23 +0900

    user model

--- ticketapp/app/tickets/page.tsx
+++ ticketapp/app/tickets/page.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import prisma from '@/prisma/db';
-import DataTable from './DataTable';
+import React from "react";
+import prisma from "@/prisma/db";
+import DataTable from "./DataTable";
 import Link from "next/link";
-import { buttonVariants } from '@/components/ui/button';
-import Pagination from '@/components/Pagination';
-import StatusFilter from '@/components/StatusFilter';
-import { Status, Ticket } from '@prisma/client';
+import { buttonVariants } from "@/components/ui/button";
+import Pagination from "@/components/Pagination";
+import StatusFilter from "@/components/StatusFilter";
+import { Status, Ticket } from "@prisma/client";
 
 export interface SearchParams {
   status: Status;
@@ -13,16 +13,16 @@ export interface SearchParams {
   orderBy: keyof Ticket;
 }
 
-const Tickets = async ({searchParams}: {searchParams: SearchParams}) => {
+const Tickets = async ({ searchParams }: { searchParams: SearchParams }) => {
   const pageSize = 10;
   const page = parseInt(searchParams.page) || 1;
   const orderBy = searchParams.orderBy ? searchParams.orderBy : "createdAt";
   const statuses = Object.values(Status);
-  const status = statuses.includes(searchParams.status) 
-    ? searchParams.status 
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
     : undefined;
 
-  let where = {}
+  let where = {};
 
   if (status) {
     where = {
@@ -30,7 +30,7 @@ const Tickets = async ({searchParams}: {searchParams: SearchParams}) => {
     };
   } else {
     where = {
-      NOT: [{status: "CLOSED" as Status}],
+      NOT: [{ status: "CLOSED" as Status }],
     };
   }
   const ticketCount = await prisma.ticket.count({ where });
@@ -45,20 +45,21 @@ const Tickets = async ({searchParams}: {searchParams: SearchParams}) => {
 
   return (
     <div>
-      <div className='flex gap-2'>
-        <Link 
-          href="/tickets/new" 
-          className={buttonVariants({variant: "default" })}
+      <div className="flex gap-2">
+        <Link
+          href="/tickets/new"
+          className={buttonVariants({ variant: "default" })}
         >
           New Ticket
         </Link>
         <StatusFilter />
       </div>
       <DataTable tickets={tickets} searchParams={searchParams} />
-      <Pagination 
-        itemCount={ticketCount} 
-        pageSize={pageSize} 
-        currentPage={page}/>
+      <Pagination
+        itemCount={ticketCount}
+        pageSize={pageSize}
+        currentPage={page}
+      />
     </div>
   );
 };
